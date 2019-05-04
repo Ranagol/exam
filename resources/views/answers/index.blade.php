767688cc454c0e7fa1968d40a59d0424c24932d4
Andor <andorhorvat@gmail.com>
2019-05-04 21:43:01 +0200

    Result table works. Scoring works

--- resources/views/answers/index.blade.php
+++ resources/views/answers/index.blade.php
@@ -6,43 +6,77 @@
 <div class="container">
     <h1>Testing the answer analyzing tool</h1>
 
+
+
+
+    <?php
+        foreach ($answers as $answer) {
+            //$superarray[$answer->username]['id'] = $answer->id; 
+            //$superarray[$answer->username]['username'] = $answer->username;
+            $superarray[$answer->id]['answ1'] = $answer->a1;
+            $superarray[$answer->id]['answ2'] = $answer->a2;
+            $superarray[$answer->id]['answ3'] = $answer->a3;
+            $superarray[$answer->id]['answ4'] = $answer->a4;
+            $superarray[$answer->id]['answ5'] = $answer->a5;
+            $superarray[$answer->id]['answ6'] = $answer->a6;
+            $superarray[$answer->id]['answ7'] = $answer->a7;
+            $superarray[$answer->id]['answ8'] = $answer->a8;
+        }
+        //echo var_dump($superarray['examadmin@gmail.com']);
+        //echo var_dump($superarray['examstudent1@gmail.com']);
+        //echo var_dump($superarray['examstudent2@gmail.com']);
+        echo var_dump($superarray);
+        echo var_dump($superarray[1]);
+        echo var_dump($superarray[2]);
+        echo var_dump($superarray[3]);
+
+
+
+        for ($i=0; $i < count($superarray); $i++) { 
+            echo 'bla bla' . '<br>';
+            $x = $i + 1;
+            $compare = array_intersect($superarray[1], $superarray[$x]);
+            echo 'Superarray[' . $x . '] has ' . count($compare) . ' correct answers.' . '<br>';
+            $correctAnswers[$x] = count($compare);
+        }
+
+        var_dump($correctAnswers);
+
+        //we need the biggest question id, in order to get the number of the questions. And we need this number for the percentage result.
+        $numberOfQuestions = $correctAnswers[1];
+        echo $numberOfQuestions;
+
+
+
+
+    ?>
+
     <table class="table">
         <tr>
             <th>username</th>
-            <th>answer a1</th>
-            <th>answer a8</th>
+            <th>Correct answers</th>
+            <th>Score (0-100)</th>
+            <th>Status</th>
         </tr>
         
         @foreach($answers as $answer)
             <tr>
                 <td>{{$answer->username}}</td>
-                <td>{{$answer->a1}}</td>
-                <td>{{$answer->a8}}</td>
+                <td>{{$correctAnswers[$answer->id]}}</td><!--number of correct answers-->
+                <td>{{100*$correctAnswers[$answer->id]/$numberOfQuestions}}</td><!--% of correct answers-->
+                <td><!--PASSED/FAILED STATUS-->
+                    <?php
+                    if (100*$correctAnswers[$answer->id]/$numberOfQuestions < 50) {
+                        echo 'FAILED';
+                    } else {
+                        echo 'PASSED';
+                    }
+                    ?>                    
+                </td><!--% of correct answers-->
             </tr>
             
         @endforeach
     </table>
-
-
-    <?php
-        //$function = "drawDice" . $numberOfDots . "Dot"
-        
-        foreach ($answers as $answer) {
-            
-            $superarray[$answer->username]['id'] = $answer->id; 
-            $superarray[$answer->username]['username'] = $answer->username;
-            $superarray[$answer->username]['answ1'] = $answer->a1;
-            $superarray[$answer->username]['answ2'] = $answer->a2;
-            $superarray[$answer->username]['answ3'] = $answer->a3;
-            $superarray[$answer->username]['answ4'] = $answer->a4;
-            $superarray[$answer->username]['answ5'] = $answer->a5;
-            $superarray[$answer->username]['answ6'] = $answer->a6;
-            $superarray[$answer->username]['answ7'] = $answer->a7;
-            $superarray[$answer->username]['answ8'] = $answer->a8;
-        }
-        echo var_dump($superarray);
-    ?>
-
     
 
     
